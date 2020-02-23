c039686655186920e62fbc6b0a007c84aac293fe
ranyasharma <52800876+ranyasharma@users.noreply.github.com>
2020-02-23 11:35:15 -0600

    Add files via upload

---
+++ APCircle.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class APCircle here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class APCircle
+{
+    private int hvalue, kvalue, radius;
+    
+    public APCircle(int h, int k, int r)
+    {
+        hvalue = h;
+        kvalue = k;
+        radius = r;
+    }
+    
+    public double getArea()
+    {
+        double area; 
+        area = Math.PI * radius * radius;
+        return area;
+    }
+    
+    public boolean isOnCircle (int x, int y)
+    {
+        if  ((x-hvalue)*(x-hvalue) + (y-kvalue)*(y-kvalue)==radius*radius)
+        return true;
+        else 
+        return false;
+    } 
+}
