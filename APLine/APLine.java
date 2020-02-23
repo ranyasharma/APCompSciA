2a0e0d13f9440843a18112ade114d2fddce50370
ranyasharma <52800876+ranyasharma@users.noreply.github.com>
2020-02-23 11:40:52 -0600

    Add files via upload

--- APLine/APLine.java
+++ APLine/APLine.java
@@ -1,1 +1,38 @@
 
+/**
+ * Write a description of class APLine here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class APLine
+{
+    private int avalue, bvalue, cvalue;
+    
+    public APLine(int a, int b, int c)
+    {
+        avalue = a;
+        bvalue = b;
+        cvalue = c;
+    }
+    
+    public double getSlope()
+    {
+        double slope;
+        slope = (-1.0 * avalue) / bvalue;
+        return slope;
+    }
+    
+    public boolean isOnLine(int x, int y)
+    {
+        if ((avalue * x) + (bvalue * y) +cvalue == 0)
+        return true;
+        else 
+        return false;
+    }
+}
+    
+    
+    
+    
+
